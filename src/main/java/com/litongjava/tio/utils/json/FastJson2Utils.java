32a82264f4f6a5734186364f449c0a19c0614486
litongjava <litongjava@qq.com>
2024-03-03 13:48:54 -1000

    release to 3.7.3.v20240301-RELEASE

---
+++ src/main/java/com/litongjava/tio/utils/json/FastJson2Utils.java
@@ -0,0 +1,30 @@
+package com.litongjava.tio.utils.json;
+
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONWriter;
+
+/**
+ * @author Tong Li
+ *
+ */
+public class FastJson2Utils {
+
+  public static String toJson(Object object) {
+    return JSON.toJSONString(object);
+  }
+
+  /**
+   * 支持传入更多 SerializerFeature
+   * 
+   * 例如：
+   *    SerializerFeature.WriteMapNullValue 支持对 null 值字段的转换
+   */
+  public static String toJson(Object object, JSONWriter.Feature... features) {
+    return JSON.toJSONString(object, features);
+  }
+
+  public static <T> T parse(String jsonString, Class<T> type) {
+    return JSON.parseObject(jsonString, type);
+  }
+
+}
