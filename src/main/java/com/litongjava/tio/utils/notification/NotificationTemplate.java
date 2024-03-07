5de607b474204aff41afffc9a2ab79107de9973a
litongjava <litongjava@qq.com>
2024-03-06 20:56:36 -1000

    change alarmTemplate

--- src/main/java/com/litongjava/tio/utils/notification/NotificationTemplate.java
+++ src/main/java/com/litongjava/tio/utils/notification/NotificationTemplate.java
@@ -2,9 +2,12 @@ package com.litongjava.tio.utils.notification;
 
 public interface NotificationTemplate {
 
-  String alarmTemplate = "**Alarm Time** : %s\n"//
-      + "**App Group Name** : %s\n" + "**App Name** : %s\n" //
-      + "**Alarm name** : %s\n" + "**Alarm Level** : %s\n" //
-      + "**Alarm Device** : %s\n" //
-      + "**Alarm Content** : %s\n";
+  String alarmTemplate = "- Alarm Time : %s\n"//
+      + "- App Group Name : %s\n" //
+      + "- App Name : %s\n" //
+      + "- Alarm name : %s\n" //
+      + "- Alarm Level : %s\n" //
+      + "- Alarm Device : %s\n" //
+      + "- Alarm Content : \n" //
+      + "%s\n";
 }
