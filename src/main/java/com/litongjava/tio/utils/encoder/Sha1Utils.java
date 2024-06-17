8643724bf4346740dd09c626e59f832d40d64f4b
litongjava <litongjava@qq.com>
2024-06-16 17:03:41 -1000

    add base64

---
+++ src/main/java/com/litongjava/tio/utils/encoder/Sha1Utils.java
@@ -0,0 +1,29 @@
+package com.litongjava.tio.utils.encoder;
+
+import java.nio.charset.Charset;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Sha1Utils {
+	
+	private final static String ALGORITHM = "SHA-1";
+	
+	public static byte[] SHA1(byte[] decript) {
+		try {
+			MessageDigest digest = java.security.MessageDigest.getInstance(ALGORITHM);
+			digest.update(decript);
+			return digest.digest();
+		} catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public static byte[] SHA1(String decript) {
+		return SHA1(decript.getBytes());
+	}
+
+	public static String SHA1(String decript, Charset encoding) {
+		byte[] array = SHA1(decript);
+		return new String(array, encoding);
+	}
+}
