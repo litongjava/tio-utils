43d420838761fb5223c4e3db187bea5b3faf98e3
litongjava <litongjava@qq.com>
2024-10-08 21:40:13 -1000

    release to 3.7.3.v20241003-RELEASE

--- src/main/java/com/litongjava/tio/utils/hutool/FilenameUtils.java
+++ src/main/java/com/litongjava/tio/utils/hutool/FilenameUtils.java
@@ -15,16 +15,39 @@ public class FilenameUtils {
     return filename.substring(dotIndex + 1);
   }
 
+  /**
+   * 获取文件名，不包含路径
+   * @param filename
+   * @return
+   */
   public static String getBaseName(String filename) {
     if (filename == null || filename.isEmpty()) {
       return "";
     }
-    // 获取文件名，不包含路径
     filename = new File(filename).getName();
     int dotIndex = filename.lastIndexOf('.');
     if (dotIndex == -1) {
       return filename;
     }
     return filename.substring(0, dotIndex);
+  }
+
+  /**
+   * 获取路径中的子路径，比如传入kb/document/md/1.md，返回kb/document/
+   * 
+   * @param path
+   * @return 子路径
+   */
+  public static String getSubPath(String path) {
+    if (path == null || path.isEmpty()) {
+      return "";
+    }
+    int lastSlashIndex = path.lastIndexOf('/');
+    if (lastSlashIndex == -1) {
+      return "";
+    }
+    // 获取最后一个斜杠之前的路径
+    String subPath = path.substring(0, lastSlashIndex + 1);
+    return subPath;
   }
 }
