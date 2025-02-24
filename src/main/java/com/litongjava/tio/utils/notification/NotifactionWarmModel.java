7e69921b3200354bbecdd7397687267d4c67ad1f
litongjava <litongjava@qq.com>
2025-02-23 23:06:15 -1000

    update notification model

--- src/main/java/com/litongjava/tio/utils/notification/NotifactionWarmModel.java
+++ src/main/java/com/litongjava/tio/utils/notification/NotifactionWarmModel.java
@@ -18,6 +18,24 @@ public class NotifactionWarmModel {
   private String warningName;
   private String level;
   private String deviceName;
+  private String requestId;
+  private String userIp;
+  private String userId;
+  private String host;
+  private String requestLine;
+  private String requestBody;
+  private String stackTrace;
   private String content;
+
+  public NotifactionWarmModel(Date time, String env, String appGroupName, String appName, String warningName, String level, String deviceName, String content) {
+    this.time = time;
+    this.appEnv = env;
+    this.appGroupName = appGroupName;
+    this.appName = appName;
+    this.warningName = warningName;
+    this.level = level;
+    this.deviceName = deviceName;
+    this.content = content;
+  }
 
 }
