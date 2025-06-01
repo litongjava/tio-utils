62c14d04aa36d1193a87dbeedd4af9735e9f6eaa
Tong Li <litongjava001@gmail.com>
2025-05-31 21:35:06 -1000

    update OkHttpClientPool

--- src/main/java/com/litongjava/tio/utils/http/OkHttpClientPool.java
+++ src/main/java/com/litongjava/tio/utils/http/OkHttpClientPool.java
@@ -28,7 +28,8 @@ public enum OkHttpClientPool {
   static okhttp3.OkHttpClient.Builder builder1200Second;
   static okhttp3.OkHttpClient.Builder builder3600Second;
   static {
-    builder30Second = new OkHttpClient().newBuilder().addInterceptor(BrotliInterceptor.INSTANCE);;
+    builder30Second = new OkHttpClient().newBuilder().addInterceptor(BrotliInterceptor.INSTANCE);
+    ;
     // 连接池
     builder30Second.connectionPool(pool());
     // 信任连接
@@ -36,7 +37,8 @@ public enum OkHttpClientPool {
     // 连接超时
     builder30Second.connectTimeout(30L, TimeUnit.SECONDS).readTimeout(30L, TimeUnit.SECONDS).build();
 
-    builder60Second = new OkHttpClient().newBuilder().addInterceptor(BrotliInterceptor.INSTANCE);;
+    builder60Second = new OkHttpClient().newBuilder().addInterceptor(BrotliInterceptor.INSTANCE);
+    ;
     // 连接池
     builder60Second.connectionPool(pool());
     // 信任连接
@@ -44,7 +46,8 @@ public enum OkHttpClientPool {
     // 连接超时
     builder60Second.connectTimeout(60L, TimeUnit.SECONDS).readTimeout(60L, TimeUnit.SECONDS).build();
 
-    builder120Second = new OkHttpClient().newBuilder().addInterceptor(BrotliInterceptor.INSTANCE);;
+    builder120Second = new OkHttpClient().newBuilder().addInterceptor(BrotliInterceptor.INSTANCE);
+    ;
     // 连接池
     builder120Second.connectionPool(pool());
     // 信任连接
@@ -61,25 +64,25 @@ public enum OkHttpClientPool {
     builder300Second.connectTimeout(300L, TimeUnit.SECONDS).readTimeout(300L, TimeUnit.SECONDS).build();
 
     //builder600Second
-    builder600Second = new OkHttpClient().newBuilder().addInterceptor(BrotliInterceptor.INSTANCE);;
+    builder600Second = new OkHttpClient().newBuilder().addInterceptor(BrotliInterceptor.INSTANCE);
+    ;
     builder600Second.connectionPool(pool());
     builder600Second.sslSocketFactory(sslSocketFactory(), x509TrustManager());
     builder600Second.connectTimeout(600L, TimeUnit.SECONDS).readTimeout(600L, TimeUnit.SECONDS).build();
 
-    builder1000Second = new OkHttpClient().newBuilder().addInterceptor(BrotliInterceptor.INSTANCE);;
+    builder1000Second = new OkHttpClient().newBuilder().addInterceptor(BrotliInterceptor.INSTANCE);
+    ;
     //builder1000Second
     builder1000Second.connectionPool(pool());
     builder1000Second.sslSocketFactory(sslSocketFactory(), x509TrustManager());
     builder1000Second.connectTimeout(1000L, TimeUnit.SECONDS).readTimeout(1000L, TimeUnit.SECONDS).build();
-
-    
 
     builder1200Second = new OkHttpClient().newBuilder();
     //builder1000Second
     builder1200Second.connectionPool(pool());
     builder1200Second.sslSocketFactory(sslSocketFactory(), x509TrustManager());
     builder1200Second.connectTimeout(1000L, TimeUnit.SECONDS).readTimeout(1000L, TimeUnit.SECONDS).build();
-    
+
     builder3600Second = new OkHttpClient().newBuilder();
     // 连接池
     builder3600Second.connectionPool(pool());
@@ -112,7 +115,7 @@ public enum OkHttpClientPool {
   public static OkHttpClient get1000HttpClient() {
     return builder1000Second.build();
   }
-  
+
   public static OkHttpClient get1200HttpClient() {
     return builder1200Second.build();
   }
